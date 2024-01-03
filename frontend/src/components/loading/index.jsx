f97e7b3b29023b519416026ed2a16188ecc3339b
AlphaHandia <118402552+AlphaHandia@users.noreply.github.com>
2024-01-04 00:53:12 +0100

    add loading components content

--- frontend/src/components/loading/index.jsx
+++ frontend/src/components/loading/index.jsx
@@ -1,4 +1,9 @@
+// PageLoader.jsx
 import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShieldAlt } from "@fortawesome/free-solid-svg-icons";
+
+
 
 const PageLoader = () => {
   const [loading, setLoading] = useState(true);
@@ -6,7 +11,7 @@ const PageLoader = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 3000);
 
     return () => {
       clearTimeout(timer);
@@ -14,23 +19,21 @@ const PageLoader = () => {
   }, []);
 
   return loading ? (
-    <div
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        background: "#000",
-        color: "#fff",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: "24px",
-        zIndex: 9999,
-      }}
-    >
-      Loading...
+    <div className="page-loader-container">
+      <div className="loader">
+        {[...Array(18)].map((_, index) => (
+          <FontAwesomeIcon
+            key={index}
+            icon={faShieldAlt}
+            className="icon"
+            style={{
+              transform: `rotate(${index * (360 / 18)}deg) translate(400px)`,
+            }}
+          />
+        ))}
+        <FontAwesomeIcon icon={faShieldAlt} className="center-icon" />
+      </div>
+  
     </div>
   ) : null;
 };
--- frontend/src/components/navbar/index.jsx
+++ frontend/src/components/navbar/index.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+// NavBar.jsx
+import React, { useState } from "react";
 import { useLanguage } from "../LanguageSelector/LanguageContext";
 import profileImage from "../../assets/images/alix-hennion.jpg";
 import LanguageSelector from "../LanguageSelector/index";
 import translations from "../../initi18n/content/translation.json";
+import PageLoader from "../loading/index"; // Assurez-vous que le chemin d'importation est correct
 
 const NavBar = () => {
   const { language } = useLanguage();
   const navbar = translations.navbar;
+  const [loading, setLoading] = useState(false);
+
+  const handleContactClick = () => {
+    setLoading(true);
+
+    // Simulez un chargement en différé (remplacez cela par votre logique de chargement réelle)
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  };
 
   return (
-    <nav className="navbar">
-      <div className="left-side">
-        <img src={profileImage} alt={navbar.profileImageAlt[language]} className="ProfileImage" />
-        <LanguageSelector />
-      </div>
-      <h1>{navbar.title[language]}</h1>
-      <div className="right-side">
-        <a href="/#Banner">{navbar.presentation[language]}</a>
-        <a href="/#mainProject">{navbar.projects[language]}</a>
-        <a href="/Contact">{navbar.contact[language]}</a>
-      </div>
-    </nav>
+    <div>
+      <PageLoader loading={loading} />
+      <nav className="navbar">
+        <div className="left-side">
+          <img
+            src={profileImage}
+            alt={navbar.profileImageAlt[language]}
+            className="ProfileImage"
+          />
+          <LanguageSelector />
+        </div>
+        <h1>{navbar.title[language]}</h1>
+        <div className="right-side">
+          <a href="/#Banner">{navbar.presentation[language]}</a>
+          <a href="/#mainProject">{navbar.projects[language]}</a>
+          <a href="/Contact" onClick={handleContactClick}>
+            {navbar.contact[language]}
+          </a>
+        </div>
+      </nav>
+    </div>
   );
 };
 
